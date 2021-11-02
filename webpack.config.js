934820291dafe3592385b5b1fe0009a56dcb04c1
unow <yunov1@gmail.com>
2021-11-02 11:39:45 +0300

    set up webpack, babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,28 @@
-module.exports = {
+const path = require("path");
 
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['@babel/preset-env', "@babel/preset-react"]
+                    }
+                },
+                exclude: /node_modules/
+            }
+        ]
+    },
+    devServer: {
+        'static': {
+            directory: './dist'
+        }
+    }
 }
