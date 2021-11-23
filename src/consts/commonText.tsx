d1ef2021b2c8eefe48f9eabe4524921ae42265e1
unow <yunov1@gmail.com>
2021-11-23 14:00:33 +0300

    extracted strings from AddEditMovieModal

--- src/consts/commonText.tsx
+++ src/consts/commonText.tsx
@@ -6,10 +6,31 @@ export default {
     buttonTitles: {
         search: "SEARCH",
         reset: "RESET",
+        submit: "SUBMIT"
     },
     deleteModal: {
         title: "Delete movie",
         message: "Are you sure you want to delete this movie?"
+    },
+    addEditModal: {
+        titles: {
+            title: "Title",
+            movieUrl: "Movie url",
+            genre: "Genre",
+            releaseDate: "Release date",
+            rating: "Rating",
+            runtime: "Runtime",
+            overview: "Overview"
+        },
+        placeholders: {
+            title: "The Matrix",
+            movieUrl: "http://",
+
+            releaseDate: "Select Date",
+            rating: "7.8",
+            runtime: "minutes",
+            overview: "More description"
+        }
     },
     common: {
         confirm: "Confirm",
